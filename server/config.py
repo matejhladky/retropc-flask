1c957d2f2a50f1f8e6283a78a8eaf39f78fce7e9
mateo <mat.hladky@gmail.com>
2024-01-23 19:49:04 +0100

    Added postgres driver

--- server/config.py
+++ server/config.py
@@ -10,8 +10,8 @@ class Config:
     # Mail settings
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'your-email@gmail.com')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'your-password')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
 
